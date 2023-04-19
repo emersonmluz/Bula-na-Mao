f77adca7043f30758535268c7d9937e7214976b1
Émerson M. Luz <emersonmluz@gmail.com>
2023-04-19 17:32:30 -0300

    Alterada imagem de background e imagem do title foi substituída por label

--- Bula-na-Mao/Bula-na-Mao/LoginViewController.swift
+++ Bula-na-Mao/Bula-na-Mao/LoginViewController.swift
@@ -13,8 +13,25 @@ final class LoginViewController: UIViewController {
         return setImageView(imageName: "Background")
     }()
     
-    lazy var titleImageView: UIImageView = {
-       return setImageView(imageName: "Title")
+    lazy var bulaTitlePartOneLabel: UILabel = {
+        let label = setLabel(text: "Bula", textColor: .systemPurple, font: "Impact", fontSize: 100)
+        label.shadowOffset = CGSize(width: 2, height: 2)
+        label.shadowColor = .black
+        return label
+    }()
+    
+    lazy var bulaTitlePartTwoLabel: UILabel = {
+        let label = setLabel(text: "Na", textColor: .systemBlue, font: "AmericanTypewriter-Bold", fontSize: 70)
+        label.shadowOffset = CGSize(width: 2, height: 2)
+        label.shadowColor = .black
+        return label
+    }()
+    
+    lazy var bulaTitlePartThreeLabel: UILabel = {
+        let label = setLabel(text: "Mão", textColor: .systemIndigo, font: "Impact", fontSize: 85)
+        label.shadowOffset = CGSize(width: 2, height: 2)
+        label.shadowColor = .black
+        return label
     }()
     
     lazy var emailTextField: UITextField = {
@@ -25,22 +42,12 @@ final class LoginViewController: UIViewController {
        return setTextField(placeHolder: "Senha")
     }()
     
-    var registerLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont(name: "Arial-BoldMT", size: 18)
-        label.text = "Não é inscrito?"
-        label.textColor = .black
-        return label
+    lazy var registerLabel: UILabel = {
+        return setLabel(text: "Não é inscrito?", textColor: .black, font: "Arial-BoldMT", fontSize: 18)
     }()
     
     lazy var registerButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Clique aqui", for: .normal)
-        button.setTitleColor(.purple, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Arial-BoldMT", size: 18)
-        return button
+        return setButton()
     }()
 
     override func viewDidLoad() {
@@ -60,7 +67,9 @@ final class LoginViewController: UIViewController {
     
     private func setComponents() {
         view.addSubview(backgroundImage)
-        view.addSubview(titleImageView)
+        view.addSubview(bulaTitlePartOneLabel)
+        view.addSubview(bulaTitlePartTwoLabel)
+        view.addSubview(bulaTitlePartThreeLabel)
         view.addSubview(emailTextField)
         view.addSubview(passwordTextField)
         view.addSubview(registerLabel)
@@ -74,12 +83,16 @@ final class LoginViewController: UIViewController {
             backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundImage.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             
-            titleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 20),
-            titleImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor,  constant: -140),
-            titleImageView.heightAnchor.constraint(equalToConstant: 300),
-            titleImageView.widthAnchor.constraint(equalToConstant: 300),
+            bulaTitlePartOneLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            bulaTitlePartOneLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,  constant: 100),
             
-            emailTextField.topAnchor.constraint(equalTo: titleImageView.bottomAnchor, constant: 50),
+            bulaTitlePartTwoLabel.centerXAnchor.constraint(equalTo: bulaTitlePartOneLabel.trailingAnchor),
+            bulaTitlePartTwoLabel.topAnchor.constraint(equalTo: bulaTitlePartOneLabel.centerYAnchor),
+            
+            bulaTitlePartThreeLabel.centerXAnchor.constraint(equalTo: bulaTitlePartTwoLabel.leadingAnchor, constant: -20),
+            bulaTitlePartThreeLabel.topAnchor.constraint(equalTo: bulaTitlePartTwoLabel.centerYAnchor, constant: -5),
+            
+            emailTextField.topAnchor.constraint(equalTo: bulaTitlePartThreeLabel.bottomAnchor, constant: 70),
             emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 70),
             emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -70),
             emailTextField.heightAnchor.constraint(equalToConstant: 50),
@@ -89,10 +102,10 @@ final class LoginViewController: UIViewController {
             passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -70),
             passwordTextField.heightAnchor.constraint(equalToConstant: 50),
             
-            registerLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            registerLabel.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 35),
             registerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -55),
             
-            registerButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -43),
+            registerButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 29),
             registerButton.leadingAnchor.constraint(equalTo: registerLabel.trailingAnchor, constant: 5)
         ])
     }
@@ -109,13 +122,31 @@ final class LoginViewController: UIViewController {
         let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.placeholder = placeHolder
-        textField.layer.cornerRadius = 8
+        textField.layer.cornerRadius = 10
         textField.backgroundColor = .white
         textField.textColor = .black
         textField.font = UIFont(name: "Arial", size: 20)
-        textField.layer.borderWidth = 0.8
+        textField.layer.borderWidth = 0.5
         textField.delegate = self
         return textField
+    }
+    
+    private func setLabel(text: String, textColor: UIColor, font: String, fontSize: Float) -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: font, size: CGFloat(fontSize))
+        label.text = text
+        label.textColor = textColor
+        return label
+    }
+    
+    private func setButton() -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Clique aqui", for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Arial-BoldMT", size: 18)
+        return button
     }
 
 }
