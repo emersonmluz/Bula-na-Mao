91b5385b9e82c54331cf9d4b0e0156f46766d684
Émerson M. Luz <emersonmluz@gmail.com>
2023-04-28 17:07:51 -0300

    implementado customButton; criado container para informações no topo da MainPage, adicionado imagem e nome do usuário na mainPage e botões histórico e favoritos;

---
+++ Bula-na-Mao/Bula-na-Mao/CustomButton.swift
@@ -0,0 +1,73 @@
+//
+//  CustomButton.swift
+//  Bula-na-Mao
+//
+//  Created by Émerson M Luz on 28/04/23.
+//
+
+import UIKit
+
+class CustomButton: UIView {
+    let button: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Arial", size: CGFloat(20))
+        button.backgroundColor = .clear
+        return button
+    }()
+    
+    private let bottonBarView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray6
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupUI() {
+        setComponents()
+        setContraints()
+    }
+    
+    private func setComponents() {
+        self.addSubview(button)
+        self.addSubview(bottonBarView)
+    }
+    
+    private func setContraints() {
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: self.topAnchor),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            button.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            
+            bottonBarView.topAnchor.constraint(equalTo: button.bottomAnchor),
+            bottonBarView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            bottonBarView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            bottonBarView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    func setTitleButton(title: String) {
+        button.setTitle(title, for: .normal)
+    }
+    
+    func isActive(_ active: Bool) {
+        if  active == true {
+            button.setTitleColor(.white, for: .normal)
+            bottonBarView.isHidden = false
+        } else {
+            button.setTitleColor(.systemGray4, for: .normal)
+            bottonBarView.isHidden = true
+        }
+    }
+}
--- Bula-na-Mao/Bula-na-Mao/Features/MainPageViewController.swift
+++ Bula-na-Mao/Bula-na-Mao/Features/MainPageViewController.swift
@@ -8,13 +8,115 @@
 import UIKit
 
 class MainPageViewController: UIViewController {
-
+    let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemTeal
+        return view
+    }()
+    
+    lazy var perfilImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "person.fill")
+        imageView.tintColor = .systemBlue
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "Arial", size: 18)
+        label.text = "Rambo Cabra Grossa e Forte"
+        label.textAlignment = .left
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = .white
+        return label
+    }()
+    
+    lazy var historyButton: CustomButton = {
+        let button = CustomButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleButton(title: "Histórico")
+        button.button.addTarget(self, action: #selector(historyButtonAction(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var favoriteButton: CustomButton = {
+        let button = CustomButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleButton(title: "Favoritos")
+        button.isActive(false)
+        button.button.addTarget(self, action: #selector(favoriteButtonAction(_:)), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        navigationController?.isNavigationBarHidden = false
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    private func setupUI() {
+        configComponents()
+        setComponents()
+        setConstraint()
+    }
+    
+    private func configComponents() {
+        view.backgroundColor = .white
+        perfilImageView.layer.cornerRadius = perfilImageView.bounds.height / 2
+    }
+    
+    private func setComponents() {
+        view.addSubview(containerView)
+        containerView.addSubview(perfilImageView)
+        containerView.addSubview(userNameLabel)
+        containerView.addSubview(historyButton)
+        containerView.addSubview(favoriteButton)
+    }
+    
+    private func setConstraint() {
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            perfilImageView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+            perfilImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            perfilImageView.heightAnchor.constraint(equalToConstant: 50),
+            perfilImageView.widthAnchor.constraint(equalToConstant: 50),
+            
+            userNameLabel.topAnchor.constraint(equalTo: perfilImageView.centerYAnchor, constant: -5),
+            userNameLabel.leadingAnchor.constraint(equalTo: perfilImageView.trailingAnchor, constant: 15),
+            userNameLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            
+            historyButton.topAnchor.constraint(equalTo: perfilImageView.bottomAnchor, constant: 10),
+            historyButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            historyButton.trailingAnchor.constraint(equalTo: containerView.centerXAnchor),
+            historyButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            historyButton.heightAnchor.constraint(equalToConstant: 60),
+            
+            favoriteButton.centerXAnchor.constraint(equalTo: historyButton.centerXAnchor),
+            favoriteButton.leadingAnchor.constraint(equalTo: containerView.centerXAnchor),
+            favoriteButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            favoriteButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            favoriteButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    
+    @objc private func historyButtonAction(_: CustomButton) {
+        historyButton.isActive(true)
+        favoriteButton.isActive(false)
+    }
+    
+    @objc private func favoriteButtonAction(_: CustomButton) {
+        favoriteButton.isActive(true)
+        historyButton.isActive(false)
     }
 }
